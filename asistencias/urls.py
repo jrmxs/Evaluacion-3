443d7a667df778b55aed7b1faf89c272822d618e
jrmxs <jrmxs92@hotmail.com>
2024-11-20 05:22:57 -0300

    cambios 1

--- asistencias/urls.py
+++ asistencias/urls.py
@@ -5,10 +5,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Landing Page
+    
     path('', views.landing_page, name='landing_page'),
-
-    # Rutas dinámicas basadas en asignaturas
     path('registrar/<str:asignatura>/', views.registrar_asistencia, name='registrar_asistencia'),
     path('listar/<str:asignatura>/', views.listar_asistencias, name='listar_asistencias'),
     path('actualizar/<str:asignatura>/<int:id>/', views.actualizar_asistencia, name='actualizar_asistencia'),
